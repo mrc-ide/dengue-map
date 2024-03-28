94e1cfb9559f8b5a329629137870c5956bdb19fe
EmmaLRussell <44669576+EmmaLRussell@users.noreply.github.com>
2024-03-28 10:59:54 +0000

    Deploying to gh-pages from @ mrc-ide/arbomap@cfbb5bbe88e1f213ebec12e6316b332c0340d4b8 🚀

---
+++ assets/about-CJC8_0nL.js
@@ -0,0 +1,1 @@
+import{a4 as a,ac as t,ai as o,ab as s,aG as r,a9 as c}from"./index-Dulrw462.js";import{u as n}from"./appStore-DW5QEuND.js";const p={key:0,class:"ma-4 text-body-1"},f={__name:"about",setup(i){const{appConfig:e}=a(n());return(m,l)=>t(e)?(c(),o("p",p,s(t(e).title)+" is in development. Check back later for more about the application. ",1)):r("",!0)}};export{f as default};
